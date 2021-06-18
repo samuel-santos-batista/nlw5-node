fdce4e448242c2ee769230a4ccf63f7515520075
samuel.batista <samuelsantxxi@gmail.com>
2021-06-18 10:43:45 -0300

    partial admin chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -45,3 +45,14 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
     });
   })
 });
+
+function sendMessage(id) {
+  const text = document.getElementById(`send_message_${id}`);
+
+ const params = {
+   text: text.value,
+   user_id: id,
+ } 
+
+ socket.emit("admin_send_message", params);
+}
--- src/websockets/admin.ts
+++ src/websockets/admin.ts
@@ -1,6 +1,5 @@
 import { io } from "../http";
 import { ConnectionsService } from '../services/ConnectionsService';
-import { UsersService } from '../services/UsersService';
 import { MessagesService } from '../services/MessagesService';
 
 interface IParams {
@@ -9,9 +8,35 @@ interface IParams {
 }
 io.on("connect", async (socket)=>{
   const connectionsService = new ConnectionsService();
- 
+  const messagesService = new MessagesService();
+
   const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
 
   io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
+
+  socket.on("admin_list_messages_by_user", async (params, callback) =>{
+    const { user_id } = params;
+
+    const allMessages = await messagesService.listByUser(user_id);
+
+    callback(allMessages); 
+  })
+
+  socket.on("admin_send_message", async (params) =>{
+    const { user_id, text } = params;
+
+    await messagesService.create({
+      text,
+      user_id,
+      admin_id: socket.id,
+    }); 
+    
+    const { socket_id } = await connectionsService.findByUserId(user_id);
+
+    io.to(socket_id).emit("admin_send_to_client", {
+      text,
+      socket_id
+    })
+  })
 });
 
